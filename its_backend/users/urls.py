5a13e228db0c075d3d44e37da02cc1f41d73348b
Shuwen <chenshuwen0210@gmail.com>
2024-02-23 12:27:13 +0800

    change url pattern for authetification component

--- its_backend/users/urls.py
+++ its_backend/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.sign_in, name='login'),
-    path('logout/', views.sign_out, name='logout'),
-    path('register/', views.sign_up, name='register'),
-    path('hello_world/', views.hello_world, name='hello_world'),
+    path('/auth/login/', views.sign_in, name='login'),
+    path('/auth/logout/', views.sign_out, name='logout'),
+    path('auth/register/', views.sign_up, name='register'),
+    path('auth/hello_world/', views.hello_world, name='hello_world'),
 ]
