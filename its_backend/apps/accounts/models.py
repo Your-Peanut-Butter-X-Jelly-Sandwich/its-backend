e897088a182bde59a39afc3adab4a57c3d19da07
명신민 <minjohn0227@gmail.com>
2024-02-29 00:11:37 +0800

    Correct check constraint into clean() method

--- its_backend/apps/accounts/models.py
+++ its_backend/apps/accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from django.db import models
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.db import models, IntegrityError
 from django.utils import timezone
 
 class CustomUserManager(BaseUserManager):
@@ -37,29 +38,28 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_student = models.BooleanField(blank=True, default=True)
     is_manager = models.BooleanField(blank=True, default=False)
 
-    class Meta:
+    # The `USERNAME_FIELD` property tells us which field we will use to log in.
+    # In this case, we want that to be the email field.
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def clean(self):
         '''
         A CustomUser can be either
             1. a superuser
             2. a manager/admin (managers can be tutors and/or students)
             3. EITHER a tutor OR a student
         '''
-        constraints = [
-            models.CheckConstraint(
-                check=(
-                    models.Q(is_superuser=True) |
-                    models.Q(is_manager=True) |
-                    (models.Q(is_tutor=True) ^ models.Q(is_student=True))
-                ),
-                name='user_role_constraint'
-            )
-        ]
-
-    # The `USERNAME_FIELD` property tells us which field we will use to log in.
-    # In this case, we want that to be the email field.
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    objects = CustomUserManager()
+        if self.is_superuser or self.is_manager:
+            return
+        if self.is_tutor ^ self.is_student:
+            return
+        
+        raise ValidationError('A user must be one of the following: ' \
+                              'superuser, ' \
+                              'manager, ' \
+                              'or either a tutor or a student')
     
     def __str__(self):
         if self.is_superuser:
