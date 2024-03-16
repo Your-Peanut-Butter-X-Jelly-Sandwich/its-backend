ab21fa61a9365690e987d06a82cbde8679baedd8
Shuwen <chenshuwen0210@gmail.com>
2024-03-16 13:34:53 +0800

    added tutor submission url

--- its_backend/apps/submissions/urls.py
+++ its_backend/apps/submissions/urls.py
@@ -1,15 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import StudentSubmissionViewSet
+from .views import StudentSubmissionViewSet, TutorSubmissionViewSet
 
-# urlpatterns = [
-#     path('student/question/<int:qn_id>/', CreateSubmissionView.as_view(), name='create_submission'),
-#     path('student/question/<int:qn_id>/past_submissions/', SubmissionHistoryView.as_view(actions={'get': 'list'}), name='submission_history'),
-# ]
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(
-    r"student/submission", StudentSubmissionViewSet, basename="student-submission"
+    r"student/submission", StudentSubmissionViewSet, basename="student-submission",
+)
+
+router.register(
+    r"tutor/submission", TutorSubmissionViewSet, basename = "tutor-submission"
 )
 
 urlpatterns = [
