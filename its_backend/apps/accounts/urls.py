430183c748bf6299f19d5897c2887c9aeaab4597
명신민 <minjohn0227@gmail.com>
2024-03-26 23:48:24 +0800

    Reorder API paths

--- its_backend/apps/accounts/urls.py
+++ its_backend/apps/accounts/urls.py
@@ -16,17 +16,18 @@ from .views import (
 )
 
 urlpatterns = [
+    path("auth/", include("allauth.urls")),
+    path("auth/change-password", ChangePasswordView.as_view(), name="change_password"),
     path("auth/login", LoginView.as_view(), name="login"),
-    path("auth/signup", SignUpView.as_view(), name="signup"),
     path("auth/logout", LogoutView.as_view(), name="logout"),
-    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/user", RetrieveUserView.as_view(), name="retrieve_user"),
+    path("auth/signup", SignUpView.as_view(), name="signup"),
     path("auth/social", SocialCallbackView.as_view(), name="social_callback"),
     path(
         "auth/social/signup/", CustomSignupView.as_view(), name="socialaccount_signup"
     ),
-    path("auth/", include("allauth.urls")),
-    path("auth/change-password", ChangePasswordView.as_view(), name="change_password"),
+    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/update-info", UpdateUserInfoView.as_view(), name="update_info"),
+    path("auth/user", RetrieveUserView.as_view(), name="retrieve_user"),
     # TODO: update API paths
     path("students", RetrieveStudentsView.as_view(), name="retrieve_students"),
     path("tutors", RetrieveTutorsView.as_view(), name="retrieve_tutors"),
@@ -35,5 +36,4 @@ urlpatterns = [
         AddTutorStudentRelationshipView.as_view(),
         name="add_tutor_student_relationship",
     ),
-    path("auth/update-info", UpdateUserInfoView.as_view(), name="update_info"),
 ]
